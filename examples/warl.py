d5f58bc9b1e0297f3a0017da5c24402b20543b29
sujeettogo <sujeettogo@gmail.com>
2019-12-17 13:48:35 +0530

    support for bitmask done

--- examples/warl.py
+++ examples/warl.py
@@ -60,128 +60,141 @@ class warl_interpreter():
         #print("input is " ,inp1)
         nodename = re.findall(r'(?:\[.+?\] -> )*\s*(.+?)\[.+?\]\s*in\s*\[.+?\]',inp1)
         nodename = list(dict.fromkeys(nodename))
-        if( len(nodename) != 1):
+        if( len(nodename) == 2 and not "bitmask" in nodename.values() ):
                 print('Wrong Syntax: Node name is different')
+                print(nodename)
                 exit()
         #print(nodename[0])
-        splits = re.findall(r'(?:{0}\[(\d.?)+:(\d.?)\])\s*in\s*\[(.+?)\]'.format(nodename[0]),inp1)
-        #print(splits)
-        for i in range(len(splits)):
-                a=int(splits[i][0])
-                b=int(splits[i][1])
-                ch.append(list(range(b,a+1)))
-                z.append(a-b+1)
-                if ":" in splits[i][2]:
-                        y=re.split("\:",splits[i][2])
-                        r1.append(int(y[0],16))         #starting ranges
-                        r2.append(int(y[1],16))         #ending ranges
-                elif "," in splits[i][2]:
-                        y=re.split("\,",splits[i][2])
-                        for a in y:
-                                l1.append(int(a,16))
-                        r1.append(l1)
-                        r2.append(l1)
-                else:
-                        r1.append(int(splits[i][2],16))
-                        r2.append(int(splits[i][2],16))
-        #print(ch)
-        for i in range(len(splits)):
-                ch2.append(int(splits[i][0]))
-                ch2.append(int(splits[i][1]))
+        if not "bitmask" in inp1: 
+                splits = re.findall(r'(?:{0}\[(\d.?)+:(\d.?)\])\s*in\s*\[(.+?)\]'.format(nodename[0]),inp1)
+                for i in range(len(splits)):
+                        a=int(splits[i][0])
+                        b=int(splits[i][1])
+                        ch.append(list(range(b,a+1)))
+                        z.append(a-b+1)
+                        if ":" in splits[i][2]:
+                                y=re.split("\:",splits[i][2])
+                                r1.append(int(y[0],16))         #starting ranges
+                                r2.append(int(y[1],16))         #ending ranges
+                        elif "," in splits[i][2]:
+                                y=re.split("\,",splits[i][2])
+                                for a in y:
+                                        l1.append(int(a,16))
+                                r1.append(l1)
+                                r2.append(l1)
+                        else:
+                                r1.append(int(splits[i][2],16))
+                                r2.append(int(splits[i][2],16))
+                #print(ch)
+                for i in range(len(splits)):
+                        ch2.append(int(splits[i][0]))
+                        ch2.append(int(splits[i][1]))
         
-        #print(ch2)
-        for i in range(len(ch)):
-                cha=cha+ch[i]  
-        for i in range(min(ch2),max(ch2)+1):
-                if i not in cha:
-                        print("Bits missing error ")
-                        exit()
-        for i in range(len(ch)):
-                for j in range(i+1,len(ch)):
-                        a_set=set(ch[i])    
-                        b_set=set(ch[j])      
-                        if(a_set & b_set):
-                                print("Overlapping error")
-                                #break
+                #print(ch2)
+                for i in range(len(ch)):
+                        cha=cha+ch[i]  
+                for i in range(min(ch2),max(ch2)+1):
+                        if i not in cha:
+                                print("Bits missing error ")
                                 exit()
-                        
-        #print(r1," ",r2)
-        #print(z)
-        #print(ch)
-        for i in z:
-                if(i%4==0):
-                        k.append(int(i/4))
-                else:
-                        k.append(int(i/4)+1)
-        #print(k)
-
-        #print(hex(r1[2][0]),hex(r1[2][1]),hex(r1[2][2]),hex(r1[2][3]))
-        #invalid input value check
-        for i in range(0,len(k)):
-                if ":" in splits[i][2]:
-                        check=re.split(":",splits[i][2])
-                        if len(check[0])-2>k[i] or len(check[1])-2>k[i]:
-                                print ("invalid range (given range exceeds)")
-                                #break
-                                exit()
-                elif isinstance(r1[i], list) == True:
-                        for p in range(len(r1[i])):
-                                if len(hex(r1[i][p]))-2>k[i]:
-                                        print("invalid comma seperated (exceeding)")
+                for i in range(len(ch)):
+                        for j in range(i+1,len(ch)):
+                                a_set=set(ch[i])    
+                                b_set=set(ch[j])      
+                                if(a_set & b_set):
+                                        print("Overlapping error")
                                         #break
                                         exit()
-                else:
-                        if len(splits[i][2])-2>k[i]:
-                                print("invalid fixed value (exceeding)")
-                                #break
-                                exit()
+                        
+                #print(r1," ",r2)
+                #print(z)
+                #print(ch)
+                for i in z:
+                        if(i%4==0):
+                                k.append(int(i/4))
+                        else:
+                                k.append(int(i/4)+1)
+                #print(k)
+
+                #print(hex(r1[2][0]),hex(r1[2][1]),hex(r1[2][2]),hex(r1[2][3]))
+                #invalid input value check
+                for i in range(0,len(k)):
+                        if ":" in splits[i][2]:
+                                check=re.split(":",splits[i][2])
+                                if len(check[0])-2>k[i] or len(check[1])-2>k[i]:
+                                        print ("invalid range (given range exceeds)")
+                                        #break
+                                        exit()
+                        elif isinstance(r1[i], list) == True:
+                                for p in range(len(r1[i])):
+                                        if len(hex(r1[i][p]))-2>k[i]:
+                                                print("invalid comma seperated (exceeding)")
+                                                #break
+                                                exit()
+                        else:
+                                if len(splits[i][2])-2>k[i]:
+                                        print("invalid fixed value (exceeding)")
+                                        #break
+                                        exit()
 
 
-        #value=str(input(" Enter hexadecimal input "))
-        sum=0
-        for i in range(len(k)):
-                sum=sum+k[i]
+                #value=str(input(" Enter hexadecimal input "))
+                sum=0
+                for i in range(len(k)):
+                        sum=sum+k[i]
                 
-        self.bitsum=sum
-        #print(sum)
-        if(len(value)>sum):
-                print("Invalid entry")
-                print("false")
-                exit()
-        elif len(value)<sum:
-                #value[0:sum-len(value)]="0"
-                for i in range(sum-len(value)):
-                        v=v+"0"
-        value=v+value
-        #print(v)   
-        #print(self.val['legal'][1])
-        for i in range(len(k)):
-                if i==0:
-                        x.append(value[0:k[i]])
-                else:
-                        x.append(value[self.prevsum(i-1,k):self.prevsum(i,k)])
-        #print(x)
-
-
-        #checking if the given input is legal or illegal
-        for i in range(len(r1)):
-                if isinstance(r1[i], list) == False: 
-                        if(int(x[i],16) in range(r1[i],r2[i]+1)):
-                                flag=1
+                self.bitsum=sum
+                #print(sum)
+                if(len(value)>sum):
+                        print("Invalid entry")
+                        print("false")
+                        exit()
+                elif len(value)<sum:
+                        #value[0:sum-len(value)]="0"
+                        for i in range(sum-len(value)):
+                                v=v+"0"
+                value=v+value
+                #print(v)   
+                #print(self.val['legal'][1])
+                for i in range(len(k)):
+                        if i==0:
+                                x.append(value[0:k[i]])
                         else:
-                                flag=0
-                                break
-                else:
-                        if int(x[i],16) in r1[i]:
-                                flag=1
+                                x.append(value[self.prevsum(i-1,k):self.prevsum(i,k)])
+                #print(x)
+        
+                for i in range(len(r1)):
+                        if isinstance(r1[i], list) == False: 
+                                if(int(x[i],16) in range(r1[i],r2[i]+1)):
+                                        flag=1
+                                else:
+                                        flag=0
+                                        break
                         else:
-                                flag=0
-                                break                
+                                if int(x[i],16) in r1[i]:
+                                        flag=1
+                                else:
+                                        flag=0
+                                        break                
+        
+        
+        else:
+                x=re.findall(r'\[\d\]\s*->\s*.*\s*\[.*\]\s*{}\s*\[(.*?,.*?)\]'.format("bitmask"),inp1)
+                z=re.findall(r'\[\d\]\s*->\s*.*\s*\[(.*)\]\s*{}\s*\[.*?,.*?\]'.format("bitmask"),inp1)
+                y1=re.split("\,",x[0])
+                bitmask=int(y1[0],16)
+                fixedval=int(y1[1],16)
+                currval=int(value,16)
+                legal=(currval&bitmask)|(~bitmask&fixedval)
+                if(hex(currval)==hex(legal)):
+                        flag=1
+                else:
+                        flag=0
+
+        
         if flag==1:
-                #print("True")
                 return True
         else:
-                #print("False")
                 return False
         
         
@@ -197,14 +210,25 @@ class warl_interpreter():
         flag2=0
         j=0
         #print(dependency_vals)
+        for i in range(len(self.val['legal'])):
+                mode1 = re.findall('\[(\d)\]\s*->\s*',self.val['legal'][i])
+                for i1 in range(len(dependency_vals)):
+                        if dependency_vals[i1] == int(mode1[i1]):
+                                j=i
+                                flag1=1
+                                break
+                        #print(mode)
+        if flag1==0:
+                print("Dependency vals do not match")
+                exit()
+        inp1=self.val['legal'][j]
+        flag1=0
         for i in range(len(self.val['wr_illegal'])):
                 mode = re.findall('\[(\d)\]\s*.*->\s*',self.val['wr_illegal'][i])
                 op=re.findall(r'in\s*\[(.*?)\]',self.val['wr_illegal'][i])
-                #print(op)
                 if op !=[]:
                         z=re.split("\:",op[0])
                 for i1 in range(len(dependency_vals)):
-                        #print(dependency_vals[i1]," ",int(mode[i1]))
                         if dependency_vals[i1] ==int(mode[i1]):
                                 if op!=[]:
                                         if int(wr_val,16) in range(int(z[0],16),int(z[1],16)):
@@ -224,11 +248,10 @@ class warl_interpreter():
         #print("input is ",inp)
         #print(dependency_vals)
         if(self.islegal(curr_val,dependency_vals) == False):
-                print("Current value should be legal")
-                return "error"
+                return "Current value must be legal"
         if(self.islegal(wr_val,dependency_vals)):
                 return wr_val
-        else:
+        elif not "bitmask" in inp1:
                 if "->" in inp:
                         op2=re.split(r'\->',inp)
                         wr=op2[1]
@@ -242,14 +265,12 @@ class warl_interpreter():
                         
                         
                 elif wr.lower().strip() == "nearup":
-                        print("nearup")
                         a=[]
                         flag2=0
                         l=self.legal(dependency_vals)
                         for i in range(len(l)):
                                 if len(l[i])==1:
                                         a.append(abs(int(wr_val,16)-int(l[i][0],16)))
-                        print(min(a))
                         for i in range(len(a)-1,-1,-1):
                                 if a[i]==min(a):
                                         j=i
@@ -262,13 +283,11 @@ class warl_interpreter():
                                  
                                         
                 elif wr.lower().strip() == "neardown":
-                        print("neardown")
                         a=[]
                         l=self.legal(dependency_vals)
                         for i in range(len(l)):
                                 if len(l[i])==1:
                                         a.append(abs(int(wr_val,16)-int(l[i][0],16)))
-                        print(min(a))
                         for i in range(len(a)):
                                 if a[i]==min(a):
                                         j=i
@@ -280,7 +299,6 @@ class warl_interpreter():
                                 
                                 
                 elif wr.lower().strip() == "nextup":
-                        print("nextup")
                         l=self.legal(dependency_vals)
                         for i in range(len(l)):
                                 #print(l[i][0])
@@ -352,7 +370,18 @@ class warl_interpreter():
                         return hex(int(wr_final,2))
  
                 else:
-                        return "Invalid update mode"               
+                        return "Invalid update mode"
+                        
+        else:
+                x=re.findall(r'\[\d\]\s*->\s*.*\s*\[.*\]\s*{}\s*\[(.*?,.*?)\]'.format("bitmask"),inp1)
+                z=re.findall(r'\[\d\]\s*->\s*.*\s*\[(.*)\]\s*{}\s*\[.*?,.*?\]'.format("bitmask"),inp1)
+                y=re.split("\,",x[0])
+                bitmask=int(y[0],16)
+                fixedval=int(y[1],16)
+                currval=int(wr_val,16)
+                legal=(currval&bitmask)|(~bitmask&fixedval)
+                return hex(legal)
+                               
         #print("update accessed successfully")
     def legal(self,dependency_vals=[]):
         '''The function takes a range(defined as a 2 tuple list) and an optional(optional incase there are no dependencies) list containing the
@@ -400,7 +429,9 @@ class warl_interpreter():
 with open(r'rv64i_isa.yaml') as file:        
         mtvec_base = warl_interpreter(yaml.load(file, Loader=yaml.FullLoader)['mtvec']['rv64']['base']['type']['WARL'])
         print(mtvec_base.dependencies(), " (dependency fields)")
-        print(mtvec_base.islegal("a00",[1])," (islegal)")
-        print(mtvec_base.islegal("a10",[1])," (islegal)")
+        print(mtvec_base.islegal("20000000",[0])," (islegal)")
+        print(mtvec_base.islegal("30000c00",[1])," (islegal)")
+        print(mtvec_base.islegal("30000c10",[1])," (islegal)")
         print(mtvec_base.legal([0])," (legal)")
-        print(mtvec_base.update("20004000","20008000",[0])," (update)")
+        print(mtvec_base.update("20000000","20008000",[0])," (update)")
+        print(mtvec_base.update("20004000","20006001",[1])," (update)")
