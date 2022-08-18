b1e1f5954406c8c6a5419cb93ebedebd88aaadda
Neel Gala <neelgala@incoresemi.com>
2022-08-18 12:12:09 +0530

    adding support for pte_ad_hw_update field (close #89)
    This is a boolean field indicating that when a virtual page is accessed and the A bit is clear, or is written and the D bit is clear, the implementation sets the corresponding bit(s) in the PTE in the hardware itself and does not raise an exception for this particular scenario. When set to False, a corresponding page-fault exception needs to be raised.

--- riscv_config/schemaValidator.py
+++ riscv_config/schemaValidator.py
@@ -144,6 +144,14 @@ class schemaValidator(Validator):
                 if not (val < maxv):
                     self._error(field, "Invalid values.")
 
+    def _check_with_s_exists(self, field, value):
+        '''Function to check that the value can be true only when 'S' mode
+        exists in the ISA string'''
+        global isa_string
+
+        if value and 'S' not in isa_string:
+            self._error(field, "cannot be set to True without 'S' mode support")
+
     def _check_with_s_check(self, field, value):
         s = 18
         check = False
