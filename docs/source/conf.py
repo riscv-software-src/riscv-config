f0721263a6ab386b18cccc35ccfd52c028d28d57
Neel Gala <neelgala@incoresemi.com>
2022-08-18 13:06:24 +0530

    remove obsolete images

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -141,7 +141,7 @@ docs_title = 'Docs / %s' %(version)
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/onlyC.png'
+#html_favicon = '_static/onlyC.png'
 html_context = {
     'show_license': html_show_license,
     'docs_title': docs_title,
