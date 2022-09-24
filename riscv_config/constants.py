715d87e77944a43fef591a4ea1ed7f0d1e3f888d
Torbjørn Viem Ness <tovine@users.noreply.github.com>
2022-09-24 12:06:23 +0200

    Refactored Z extensions into a list which is then joined into the isa_regex string to avoid repetition

--- riscv_config/constants.py
+++ riscv_config/constants.py
@@ -6,5 +6,45 @@ root = os.path.abspath(os.path.dirname(__file__))
 isa_schema = os.path.join(root, 'schemas/schema_isa.yaml')
 debug_schema = os.path.join(root, 'schemas/schema_debug.yaml')
 platform_schema = os.path.join(root, 'schemas/schema_platform.yaml')
+Z_extensions = [
+        "Zicbom",
+        "Zicbop",
+        "Zicboz",
+        "Zicsr",
+        "Zifencei",
+        "Zihintpause",
+        "Zam",
+        "Zfinx",
+        "Zfh",
+        "Zdinx",
+        "Zhinx",
+        "Zhinxmin",
+        "Ztso",
+        "Zba",
+        "Zbb",
+        "Zbc",
+        "Zbe",
+        "Zbf",
+        "Zbkb",
+        "Zbkc",
+        "Zbkx",
+        "Zbm",
+        "Zbp",
+        "Zbr",
+        "Zbs",
+        "Zbt",
+        "Zk",
+        "Zkn",
+        "Zknd",
+        "Zkne",
+        "Zknh",
+        "Zkr",
+        "Zks",
+        "Zksed",
+        "Zksh",
+        "Zkt",
+        "Zmmul",
+        "Svnapot"
+]
 isa_regex = \
-        re.compile("^RV(32|64|128)[IE][ACDFGHJLMNPQSTUVX]*((Zicbom|Zicbop|Zicboz|Zicsr|Zifencei|Zihintpause|Zam|Zfinx|Zfh|Zdinx|Zhinx|Zhinxmin|Ztso|Zba|Zbb|Zbc|Zbe|Zbf|Zbkb|Zbkc|Zbkx|Zbm|Zbp|Zbr|Zbs|Zbt|Zk|Zkn|Zknd|Zkne|Zknh|Zkr|Zks|Zksed|Zksh|Zkt|Zmmul|Svnapot)(_(Zicbom|Zicbop|Zicboz|Zicsr|Zifencei|Zihintpause|Zam|Zfinx|Zfh|Zdinx|Zhinx|Zhinxmin|Ztso|Zba|Zbb|Zbc|Zbe|Zbf|Zbkb|Zbkc|Zbkx|Zbm|Zbp|Zbr|Zbs|Zbt|Zk|Zkn|Zknd|Zkne|Zknh|Zkr|Zks|Zksed|Zksh|Zkt|Zmmul|Svnapot))*){,1}$")
+        re.compile("^RV(32|64|128)[IE][ACDFGHJLMNPQSTUVX]*(("+'|'.join(Z_extensions)+")(_("+'|'.join(Z_extensions)+"))*){,1}$")
